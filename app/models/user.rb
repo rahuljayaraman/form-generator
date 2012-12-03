2d33fc2e05e50bda1976f9822e746ad5b4cdce7b
Rahul Jayaraman <recklessrahul@gmail.com>
2012-12-03 10:51:39 +0530

    User scaffolding

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User
+  include Mongoid::Document
+  authenticates_with_sorcery!
+
+  field :name, type: String
+  field :email, type: String
+  field :crypted_password, type: String
+  field :salt, type: String
+
+  attr_accessible :name, :email, :password, :password_confirmation
+
+  validates_length_of :password, :minimum => 5, :message => "password must be at least 5 characters long", :if => :password
+  validates_confirmation_of :password, :message => "should match confirmation", :if => :password
+  validates_uniqueness_of :email
+end
