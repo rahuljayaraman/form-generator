ce8145be33da584437cbae40cbb248a632fe5477
Rahul Jayaraman <recklessrahul@gmail.com>
2012-12-03 10:48:51 +0530

    Factory to create a User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :require_login
+
+  protected
+  def not_authenticated
+      redirect_to login_path, :alert => "You're not authorized to access this page. Please login first."
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,12 +8,16 @@ Spork.prefork do
   require 'capybara/rspec'
 
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  DatabaseCleaner[:mongoid].strategy = :truncation
 
   RSpec.configure do |config|
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
     config.use_transactional_fixtures = false
     config.infer_base_class_for_anonymous_controllers = false
     config.order = "random"
+    config.filter_run focus: true
+    config.run_all_when_everything_filtered = true
+    config.include Mongoid::Matchers
     config.include FactoryGirl::Syntax::Methods
     config.include Capybara::DSL
     ActiveSupport::Dependencies.clear
