708b1ee2a178924bbf6bc99172c64657f90976aa
Rahul Jayaraman <recklessrahul@gmail.com>
2012-12-04 18:51:58 +0530

    Validate uniqueness of set_name scoped to user

--- app/models/source.rb
+++ app/models/source.rb
@@ -16,6 +16,7 @@ class Source
   belongs_to :user
 
   validates_presence_of :user_id, :set_name
+  validates_uniqueness_of :set_name, scope: :user_id
 
   def initialize_set
     model_name = self.set_name.classify
--- spec/models/source_spec.rb
+++ spec/models/source_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe Source do
   it { should have_fields(:set_name) }
   it { should belong_to(:user) }
-  it { should validate_uniquess_of(:set_name).scoped_to(:user) }
+  it { should validate_uniqueness_of(:set_name).scoped_to(:user_id) }
 
   it "should always be associated with a user" do
     build(:source, user_id: "").should_not be_valid
   end
 
-  context "Dynamic Model", focus: true do
+  context "Dynamic Model" do
     let(:set) { create(:source) }
     let(:klass) { set.initialize_set }
     subject { klass.wrap }
