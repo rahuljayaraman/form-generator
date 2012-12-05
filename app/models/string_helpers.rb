2c46adb700bc174a330fe5804b3d49171b4c4d5b
Rahul Jayaraman <recklessrahul@gmail.com>
2012-12-05 09:58:08 +0530

    Try is not required as even nil classes would raise NoMethod. Rescuing NoMethod for now, although this does not seem necessary as model names would be unique.

--- app/models/string_helpers.rb
+++ app/models/string_helpers.rb
@@ -1,7 +1,10 @@
 class String
   def not_loaded?
-    false if eval(self).try(:wrap).exists?
+    false if eval(self).wrap.exists?
   rescue NameError
+    true
+  rescue NoMethodError
+   # This would be applicable for reserved or nil classes i presume. I don't expect this usecase to come up though.
     true
   end
 end
