d29be9cc420387af6accf856f643b26a91d02e62
Rahul Jayaraman <recklessrahul@gmail.com>
2012-12-13 16:21:38 +0530

    Sorting for datatables. Need to find a better way to generalize this by defining sorting in the view maybe

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,6 +23,6 @@ $(document).ready(function(){
   $('#datatables').dataTable({
     "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
     "sPaginationType": "bootstrap",
-    "aaSorting": [[ 5, "desc" ]]
+    "aaSorting": [[ 1, "desc" ]]
   });
 });
