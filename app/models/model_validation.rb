75fa702f8d6baf815edda258eb83bf2d1f81f3fd
Rahul Jayaraman <recklessrahul@gmail.com>
2012-12-28 17:49:12 +0530

    Validation types added

--- app/models/model_validation.rb
+++ app/models/model_validation.rb
@@ -2,7 +2,15 @@ class ModelValidation
   include Mongoid::Document
 
   field :validation_type, type: String
-  field :validation_options, type: Array
+  field :min, type: Integer
+  field :max, type: Integer
+  field :message, type: String
+
+  attr_accessible :validation_type, :min, :max, :message
 
   embedded_in :source_attribute
+
+  def self.show
+    ['Presence', 'Uniqueness', 'Length']
+  end
 end
--- spec/models/model_validation_spec.rb
+++ spec/models/model_validation_spec.rb
@@ -2,5 +2,10 @@ require 'spec_helper'
 
 describe ModelValidation do
   it { should be_embedded_in(:source_attribute) }
-  it { should have_fields(:validation_type, :validation_options) }
+  it { should have_fields(:validation_type, :min, :max, :message) }
+
+  it "should return validation types expected by the system" do
+    array = ['Presence', 'Uniqueness', 'Length']
+    ModelValidation.show.should == array
+  end
 end
