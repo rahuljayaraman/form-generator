6c45286c2c32594fe362a1aead87f918ab57518e
Rahul Jayaraman <recklessrahul@gmail.com>
2013-01-01 19:05:26 +0530

    Create two-way relationships between sources

--- app/models/source.rb
+++ app/models/source.rb
@@ -9,16 +9,13 @@ class Source
   has_many :source_attributes, dependent: :destroy
   belongs_to :user
 
-  has_and_belongs_to_many :has_manies, class_name: "Source", inverse_of: :has_many_used_in
-  has_and_belongs_to_many :has_many_used_in, class_name: "Source", inverse_of: :has_manies
-  
-  has_and_belongs_to_many :belongs_tos, class_name: "Source", inverse_of: :belongs_to_used_in
-  has_and_belongs_to_many :belongs_to_used_in, class_name: "Source", inverse_of: :belongs_tos
+  has_and_belongs_to_many :has_manies, class_name: "Source", inverse_of: :belongs_tos, dependent: :nullify
+  has_and_belongs_to_many :belongs_tos, class_name: "Source", inverse_of: :has_manies, dependent: :nullify
 
   accepts_nested_attributes_for :source_attributes, :allow_destroy => true
 
   validates_presence_of :user_id, :source_name
   validates_uniqueness_of :source_name, scope: :user_id
 
-  attr_accessible :source_name, :user_id, :source_attributes_attributes
+  attr_accessible :source_name, :user_id, :source_attributes_attributes, :has_many_ids, :belongs_to_ids
 end
