08f32911ab492fd5538a93246cb49610b9b830a4
Rahul Jayaraman <recklessrahul@gmail.com>
2013-01-04 18:37:21 +0530

    Define relationships for related dynamic models. Not sure if this is necessary. Might remove later if not required.

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,12 @@ class User
   validates_length_of :password, :minimum => 5, :message => "password must be at least 5 characters long", :if => :password
   validates_confirmation_of :password, :message => "should match confirmation", :if => :password
   validates_uniqueness_of :email
+
+  def self.define_relationships associated
+    associated.each do |model|
+      User.has_many model.collection_name.to_sym
+      model.belongs_to :user
+    end
+  end
+
 end
