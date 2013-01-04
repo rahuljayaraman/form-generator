5bab0f955390205954251a1bc3f7a3c1a16364da
Rahul Jayaraman <recklessrahul@gmail.com>
2013-01-04 12:38:07 +0530

    Form Builder completed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,13 +16,67 @@
 //= require bootstrap-datepicker
 //= require dataTables/jquery.dataTables
 //= require dataTables/jquery.dataTables.bootstrap
-//= require jquery.ui.all
-//= require ui.multiselect.js
+//= require jquery.ui.sortable
 //= require jquery_nested_form
 //= require_tree .
 
+
 $(document).ready(function(){
-  $(".multiselect").multiselect();
+  $("#selected_items").sortable({
+    axis: 'y',
+    stop: function(event, ui) {
+      var sorting = $(this).sortable("toArray");
+      setPosition(sorting);
+    }
+  });
+  var setPosition = function(sorting) {
+    $(".checkboxes").each(function(index) {
+       var position = index + 1;
+       $(this).append("<input class='hidden' id='form_priority' name='[form][form_attributes_attributes][" + sorting[index] + "][priority]' type='hidden' value='" + position + "'>");
+    });
+  };
+
+  $(".unselected").live("click", function(e) {
+    e.preventDefault();
+    addElement($(this));
+    $(this).fadeOut("slow").remove();
+  });
+
+  $(".remove").live("click", function(e) {
+    e.preventDefault();
+    var element = $(this).parent("div");
+    removeElement(element);
+    element.fadeOut("slow").remove();
+  });
+
+  var removeElement = function(element) {
+    var newElement = "<a href='#' class='icon-arrow-left unselected' data_type='" + element.attr('data_type') + "' id='" + element.attr('id') + "'> " + element.find('a:first').text().trim() +"</a>";
+    if(element.attr('data_type').trim() == "normal") {
+      $(".normal").append(newElement);
+    }
+    else if(element.attr('data_type').trim() == "many") {
+      $(".many").append(newElement);
+    }
+    else {
+      $(".embedded").append(newElement);
+    }
+  };
+
+  var addElement = function(element) {
+    var position = fetchPosition() + 1;
+    var value = "<small class='muted'><b>[drag]</b></small><a href='#' class='' id='" + element.text().trim() + "'>  " + element.text().trim() +"&nbsp;&nbsp;</a>" + "<a href='#' class='icon-remove muted remove'><small> remove </small></a>";
+    var hiddenPosition = "<input class='hidden' id='form_priority' name='[form][form_attributes_attributes][" + element.attr("id") + "][priority]' type='hidden' value='" + position + "'>";
+    var hiddenSource = "<input class='hidden' id='form_source' name='[form][form_attributes_attributes][" + element.attr("id") + "][source_attribute_id]' type='hidden' value='" + element.attr("id") + "'>";
+    var openTag = "<div class='checkboxes' id='" + element.attr("id") + "' data_type='" + element.attr('data_type') + "'>";
+    var closeTag = "</div>";
+    var newElement = openTag + value + hiddenPosition + hiddenSource + closeTag;
+    $("#selected_items").append(newElement);
+  };
+
+  var fetchPosition = function() {
+    var length = $("#selected_items").find("li.checkboxes").length;
+    return length;
+  };
   $(".range").hide();
 
   //Toggle Show validations for sources/edit
--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -7,19 +7,22 @@ class FormsController < ApplicationController
   def new
     if @source = Source.find(params[:source])
       @form = current_user.forms.new
-      @source_attributes = @source.source_attributes
-      @related_has_manies = @source.has_manies
-      @related_belongs_tos = @source.belongs_tos
+      @available_source_attributes = @source.source_attributes
+      @available_has_manies = @source.has_manies
+      @available_belongs_tos = @source.belongs_tos
     end
   end
 
   def edit
-    if @source = Source.find(params[:source])
-      @form = current_user.forms.find(params[:id])
-      @source_attributes = @source.source_attributes
-      @related_has_manies = @source.has_manies
-      @related_belongs_tos = @source.belongs_tos
-    end
+    @form = current_user.forms.find(params[:id])
+    @source = @form.source
+    @source_attributes = @source.source_attributes
+    @related_has_manies = @source.has_manies
+    @related_belongs_tos = @source.belongs_tos
+    @available_source_attributes = @source_attributes - @form.source_attributes
+    @available_has_manies = @related_has_manies - @form.source_attributes
+    @available_belongs_tos = @related_belongs_tos - @form.source_attributes
+    @source_attribute_ids = @form.source_attributes.map(&:id)
   end
 
   def show
@@ -29,7 +32,7 @@ class FormsController < ApplicationController
   def create
     @form = current_user.forms.new params[:form]
     if @form.save
-      redirect_to user_path(current_user), notice: "Form saved"
+      redirect_to form_path(@form), notice: "Form saved"
     else
       render :new
     end
@@ -38,14 +41,12 @@ class FormsController < ApplicationController
   def update
     @form = current_user.forms.find(params[:id])
 
-    respond_to do |format|
-      if @form.update_attributes(params[:form])
-        format.html { redirect_to @form, notice: 'Form was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @form.errors, status: :unprocessable_entity }
-      end
+    # This line makes me want to puke. Unfortunately, find_or_initialize may not work for nested_attributes. Will wrap this in a transaction later.
+    @form.form_attributes.destroy_all
+    if @form.update_attributes(params[:form])
+      redirect_to form_path(@form), notice: 'Form was successfully updated.' 
+    else
+      render action: "edit" 
     end
   end
 
