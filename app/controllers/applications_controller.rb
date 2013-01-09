87bfa85e8479c09d70bde3f9a07db2844844982a
Rahul Jayaraman <recklessrahul@gmail.com>
2013-01-09 11:38:10 +0530

    Add roles from Applications's dashboard

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -14,6 +14,8 @@ class ApplicationsController < ApplicationController
   # GET /applications/1.json
   def show
     @application = current_user.owned_applications.find(params[:id])
+    @role = @application.roles.new
+    @roles = @application.roles.all
 
     respond_to do |format|
       format.html # show.html.erb
