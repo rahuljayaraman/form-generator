f1531c81309a3728d71595d1c3a5b41b310daaf1
Rahul Jayaraman <recklessrahul@gmail.com>
2013-01-12 09:41:23 +0530

    UI changes

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,14 +6,10 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    respond_to do |format|
-      if @user = login(params[:email],params[:password])
-        format.html { redirect_back_or_to(user_path(@user), :notice => 'Login successful.') }
-        format.xml { render :xml => @user, :status => :created, :location => @user }
-      else
-        format.html { flash.now[:alert] = "Login failed."; render :action => "new" }
-        format.xml { render :xml => @user.errors, :status => :unprocessable_entity }
-      end
+    if @user = login(params[:email],params[:password])
+      redirect_back_or_to(user_path(@user), :notice => 'Login successful.') 
+    else
+      redirect_back_or_to(root_path, :alert => "Login failed!")
     end
   end
 
