845b768a5594485b463832ea136f7bba040f845d
Rahul Jayaraman <recklessrahul@gmail.com>
2013-01-13 17:28:49 +0530

    Compiling assets locally & some more production settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,25 +50,46 @@ namespace :deploy do
     end
   end
 
-  namespace :mongoid do
-    desc "Copy mongoid config"
-    task :copy do
-      upload "config/mongoid/#{rails_env}.yml", "#{shared_path}/mongoid.yml", :via => :scp
+  namespace :assets do
+    task :precompile, :roles => :web do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
+        run_locally("rake assets:clean && rake assets:precompile")
+        run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+        top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
+        run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
+        run_locally "rm public/assets.tar.bz2"
+        run_locally("rake assets:clean")
+      else
+        logger.info "Skipping asset precompilation because there were no asset changes"
+      end
     end
 
-    desc "Link the mongoid config in the release_path"
-    task :symlink do
-      run "test -f #{release_path}/config/mongoid.yml || ln -s #{shared_path}/mongoid.yml #{release_path}/config/mongoid.yml"
-    end
-
-    desc "Create MongoDB indexes"
-    task :index do
-      run "cd #{current_path} && #{bundle_cmd} exec rake db:mongoid:create_indexes", :once => true
+    task :symlink, roles: :web do
+      run ("rm -rf #{latest_release}/public/assets &&
+                                     mkdir -p #{latest_release}/public &&
+                                                 mkdir -p #{shared_path}/assets &&
+                                                             ln -s #{shared_path}/assets #{latest_release}/public/assets")
     end
   end
    
   after "deploy:update_code", "mongoid:copy"
   after "deploy:update_code", "mongoid:symlink"
   after "deploy:update", "mongoid:index"
+  before 'deploy:finalize_update', 'deploy:assets:symlink'
+  after 'deploy:update_code', 'deploy:assets:precompile'
   before "deploy", "deploy:check_revision"
+end
+namespace :mongoid do
+  task :copy do
+    upload "config/mongoid/#{rails_env}.yml", "#{shared_path}/mongoid.yml", :via => :scp
+  end
+
+  task :symlink do
+    run "test -f #{release_path}/config/mongoid.yml || ln -s #{shared_path}/mongoid.yml #{release_path}/config/mongoid.yml"
+  end
+
+  task :index do
+    run "cd #{current_path} && bundle exec rake db:mongoid:create_indexes", :once => true
+  end
 end
