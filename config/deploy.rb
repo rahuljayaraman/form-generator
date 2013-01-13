b4f6fb81ff5f264fd5b7818db4d143c6a3c3226f
Rahul Jayaraman <recklessrahul@gmail.com>
2013-01-13 17:00:13 +0530

    Deploy config for mongoid

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,5 +49,26 @@ namespace :deploy do
       exit
     end
   end
+
+  namespace :mongoid do
+    desc "Copy mongoid config"
+    task :copy do
+      upload "config/mongoid/#{rails_env}.yml", "#{shared_path}/mongoid.yml", :via => :scp
+    end
+
+    desc "Link the mongoid config in the release_path"
+    task :symlink do
+      run "test -f #{release_path}/config/mongoid.yml || ln -s #{shared_path}/mongoid.yml #{release_path}/config/mongoid.yml"
+    end
+
+    desc "Create MongoDB indexes"
+    task :index do
+      run "cd #{current_path} && #{bundle_cmd} exec rake db:mongoid:create_indexes", :once => true
+    end
+  end
+   
+  after "deploy:update_code", "mongoid:copy"
+  after "deploy:update_code", "mongoid:symlink"
+  after "deploy:update", "mongoid:index"
   before "deploy", "deploy:check_revision"
 end
