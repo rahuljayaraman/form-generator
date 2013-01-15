adfc3834169effe2ee7e9d91085c0cd6006e4584
Rahul Jayaraman <recklessrahul@gmail.com>
2013-01-15 21:52:12 +0530

    Try to speed up test suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,14 +6,11 @@ Spork.prefork do
   require 'rspec/rails'
   require 'rspec/autorun'
   require 'capybara/rspec'
-  require 'capybara/poltergeist'
 
-  Capybara.javascript_driver = :poltergeist
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
   DatabaseCleaner[:mongoid].strategy = :truncation
 
   RSpec.configure do |config|
-    config.use_transactional_fixtures = false
     config.infer_base_class_for_anonymous_controllers = false
     config.order = "random"
     config.filter_run focus: true
@@ -28,16 +25,14 @@ end
 
 Spork.each_run do
   Fabrication.clear_definitions
-  # Dir[File.join(File.dirname(__FILE__), '..', 'app', 'helpers', '*.rb')].each do |file|
-  #   require file
-  # end
   RSpec.configure do |config|
     config.before(:each) do
-      DatabaseCleaner.start
-    end
-
-    config.after(:each) do
+      # DatabaseCleaner.start
       DatabaseCleaner.clean
     end
+# 
+#     config.after(:each) do
+#       DatabaseCleaner.clean
+#     end
   end
 end
