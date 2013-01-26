7773fa71c51ee81f8cb2c73042c74c48862fad3b
Rahul Jayaraman <recklessrahul@gmail.com>
2013-01-26 15:10:59 +0530

    Import headers or master data

--- app/models/source.rb
+++ app/models/source.rb
@@ -78,4 +78,44 @@ class Source
   def habtms_attributes
     habtms.map(&:source_attributes).inject([]){|initial,sum| initial + sum}
   end
+
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+    when '.csv' then Csv.new(file.path, nil, :ignore)
+    when '.xls' then Excel.new(file.path, nil, :ignore)
+    when '.xlsx' then Excelx.new(file.path, nil, :ignore)
+    else raise "Unknown file type: #{file.original_filename}"
+    end
+  end
+
+  def self.import_headers(file, row)
+    spreadsheet = open_spreadsheet(file)
+    header = spreadsheet.row(row)
+  end
+
+  def self.import_data(name, file, user)
+    spreadsheet = open_spreadsheet(file)
+    header = spreadsheet.row(1)
+    guess_type = spreadsheet.row(2)
+    source = user.sources.new(source_name: name)
+    header.each_with_index do |value, index|
+      case guess_type[index].class.to_s
+      when "String"
+        type = "String"
+      when "Integer", "Float"
+        type = "Number"
+      end
+      source.source_attributes.build(field_name: value, field_type: type)
+    end
+    source.save
+    header = header.collect { |val| val.gsub(/[^0-9a-zA-Z]/i, '').underscore }
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      model = source.initialize_dynamic_model
+      User.define_relationships [model]
+      data = user.send(model.collection_name).new(row.to_hash)
+      data.save
+    end
+    source
+  end
 end
