5ed81146aa1690a3bd5b4d81c42893365ae34c59
Rahul Jayaraman <recklessrahul@gmail.com>
2013-01-29 12:45:59 +0530

    Fix bug: Login page being show even after login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_filter :require_login
 
   def home
+    redirect_to user_path(current_user) if current_user
     @user = User.new
   end
 end
